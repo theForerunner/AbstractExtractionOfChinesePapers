368cfaaad0468cd06df3bfa067a96538b5e6bbba
Siennaaaa <sekhn99@163.com>
2020-04-09 21:22:25 +0800

    18-19年文献摘要提取
    基本功能实现，但关于category code的处理仍待优化。header_format_18是用于处理没有category code文献的

--- pdf_parser.py
+++ pdf_parser.py
@@ -10,10 +10,11 @@ par_dir = os.getcwd()
 input_path = os.path.join(par_dir, 'input')
 output_path = os.path.join(par_dir, 'output')
 header_format_path = os.path.join(par_dir, 'header_format.json')
+header_format_path_2 = os.path.join(par_dir, 'header_format_18.json')
 
 journal_list = ['journal_of_software', 'journal_of_computers','data']
 journal_name = journal_list[2]
-file_name = '18001.pdf'
+file_name = '19001.pdf'
 
 pdf_path = os.path.join(input_path, journal_name, file_name)
 txt_name = file_name.replace('.pdf', '.txt')
@@ -95,9 +96,9 @@ def txt_parser(path, target_path, header_format_path):
             header_mapper = eval(''.join(fp.readlines()))
         return header_mapper
 
-    headers = header_format_loader(header_format_path)
-    header_key_list = list(headers.keys())
-    print(header_key_list)
+    # headers = header_format_loader(header_format_path)
+    # header_key_list = list(headers.keys())
+
     info = {}
 
     # 读取 PDF 提取结果
@@ -106,43 +107,59 @@ def txt_parser(path, target_path, header_format_path):
     line_ptr = 0
     line_count = len(lines)
 
+    isCateC = 0
+
+    for i in range(line_count):
+        if lines[i].startswith('中图法分类号'):
+            isCateC = 1
+            break;
+
+    if isCateC == 0:
+        #没有 category code
+        headers = header_format_loader(header_format_path_2)
+    else :
+        headers = header_format_loader(header_format_path)
+
+    header_key_list = list(headers.keys())
+
     citation_url = ''
     content_start = 0
 
     # 分别提取各部分信息
     info['title'] = lines[0].strip()
 
+    #提取作者
     # info['authors'] = re.sub('[0-9,]+', ' ', re.sub(r'[\s]+', '', ''.join(lines[1:3]))).split() [sample_1 avaliable]
     info['authors'] = re.sub('[0-9,]+', ' ', re.sub(r'[\s]+', '', ''.join(lines[1:2]))).split()
 
-    # print(len(lines)),不是lines超出了，可能是key
+
+
     for index, key in enumerate(header_key_list):
-        print(key)
-        #如果每一行的开头不是论文中标识的内容的时候
-        # while not lines[line_ptr].startswith(headers[key]['content']):
-        #     line_ptr += 1
+
+        # if lines[line_ptr].startswith(headers['keywords']['content']):
+        #     continue
+        while line_ptr<line_count and not lines[line_ptr].startswith(headers[key]['content']) :
+                line_ptr += 1
         line_start = line_ptr
-        print(line_start)
 
-
-        for i,val in enumerate(lines,line_start):
-            # print(val)
-            if not val.startswith(headers[key]['content']):
-                line_ptr+=1;
-            else:
-                print("stop!")
-                break;
         try:
-            #摘要的上一行一般是通信作者
-            if key =='abstract' :
-                info['corresponding_author']=lines[line_start-1].strip("通讯作者: ")
+            if key == 'corresponding_author' :
+                info['corresponding_author'] = lines[line_ptr].strip("\n")[
+                                               headers['corresponding_author']['length']:]
+                line_ptr += 1
+            if key == 'abstract' :
+                #摘要的后面是关键词
+                while line_ptr < line_count and not lines[line_ptr].startswith(headers['keywords']['content']):
+                    line_ptr += 1
+                info['keywords'] = re.sub('[0-9,]+', ' ', re.sub(r'[\s]+', '', ''.join(lines[line_ptr]))).split()
+
             if key == 'citation_en':
                 # 由于与中文的引用一样, 以 URL 结尾, 检测完整URL是否存在
                 while not ''.join(lines[line_start:line_ptr]).replace('\n', '').strip()[-len(citation_url):] == citation_url:
                     line_ptr += 1
-            elif key == 'keywords_en':
+            if key == 'keywords_en':
                 # 由于英文的关键词在中文正文的最上方, 因此检测下一行是否有中文(过滤掉英文及标点符号后不为空)
-                while not re.sub(r'[0-9a-zA-Z:;\-\s]+', '', lines[line_ptr]):
+                while not re.sub(r'[0-9a-zA-Z.:;\-\s]+', '', lines[line_ptr]):
                     line_ptr += 1
                 content_start = line_ptr
             elif key == 'references':
@@ -152,16 +169,21 @@ def txt_parser(path, target_path, header_format_path):
             else:
                 while not lines[line_ptr].startswith(headers[header_key_list[index + 1]]['content']):
                     line_ptr += 1
+
         except IndexError:
             line_ptr += 1
         info[key] = ''.join([line.strip() for line in lines[line_start:line_ptr]])[headers[key]['length']:]
+        # print(line_start)
+        # print(key)
+        # print(line_ptr)
+        # print(info[key])
         if key == 'citation':
             # 提取中文引用中的 url
             citation_url = info[key][info[key].find('http'):]
         elif key in ['keywords', 'keywords_en']:
             # 切分关键词
             info[key] = info[key].replace('; ', ';').split(';')
-        print(line_ptr)
+
 
     with open(target_path, 'w', encoding='utf-8') as fw:
         json.dump(info, fw, ensure_ascii=False, indent=4)
@@ -170,3 +192,18 @@ def txt_parser(path, target_path, header_format_path):
 if __name__ == '__main__':
     pdf2text(pdf_path, txt_path)
     txt_parser(txt_path, json_path, header_format_path)
+    for i in range(3):
+        file_name=file_name.split(".")
+        file_name[0] = int(file_name[0])+1
+        file_name[0] = str(file_name[0])
+        file_name = ".".join(file_name)
+        pdf_path = os.path.join(input_path, journal_name, file_name)
+        txt_name = file_name.replace('.pdf', '.txt')
+        json_name = file_name.replace('.pdf', '.json')
+        txt_path = os.path.join(output_path, journal_name, txt_name)
+        json_path = os.path.join(output_path, journal_name, json_name)
+
+        pdf2text(pdf_path, txt_path)
+        txt_parser(txt_path, json_path, header_format_path)
+
+
